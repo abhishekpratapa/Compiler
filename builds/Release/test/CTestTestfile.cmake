f7c314f382a8eaa9d07dde0c19b4385451921d5b
Romi <romieklund@gmail.com>
2019-06-29 13:50:35 -0700

    initial commit

--- builds/Release/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/romieklund/Desktop/Github/Compiler/test
-# Build directory: /Users/romieklund/Desktop/Github/Compiler/builds/Release/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test1 "/Users/romieklund/Desktop/Github/Compiler/target/tests/test_executable")
