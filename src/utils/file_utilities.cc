91500064bd20e03313771898dcfddf3afae3296a
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-07-09 04:07:43 -0700

    changed namespacing

--- src/utils/file_utilities.cc
+++ src/utils/file_utilities.cc
@@ -1,8 +1,14 @@
 #include <utils/file_utilities.h>
 
-std::string acc::utils::read_file_contents(std::string &file_name) {
+namespace acc {
+namespace utils {
+
+std::string read_file_contents(std::string &file_name) {
   std::ifstream ifs(file_name.c_str());
   std::string data((std::istreambuf_iterator<char>(ifs)),
                    (std::istreambuf_iterator<char>()));
   return data;
 }
+
+} // namespace utils
+} // namespace acc
