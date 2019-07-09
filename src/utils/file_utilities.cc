92a4cafb6742a543325b5173ddb081d08b813fbf
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-07-09 04:06:04 -0700

    changed std::string input to c_str input

--- src/utils/file_utilities.cc
+++ src/utils/file_utilities.cc
@@ -1,7 +1,7 @@
 #include <utils/file_utilities.h>
 
 std::string acc::utils::read_file_contents(std::string &file_name) {
-  std::ifstream ifs(file_name);
+  std::ifstream ifs(file_name.c_str());
   std::string data((std::istreambuf_iterator<char>(ifs)),
                    (std::istreambuf_iterator<char>()));
   return data;
