b48eef66a3713ee9ae9bd148a411626a9e16be2c
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-07-22 23:43:47 -0700

    Actually got Code Coverage to work

---
+++ test/test_lexical_analyzer.cc
@@ -0,0 +1,22 @@
+#include "test_main.h"
+
+#define BOOST_TEST_MODULE main
+
+#include <boost/test/unit_test.hpp>
+#include <utils/argument_parser.h>
+#include <lexical_analyzer.h>
+
+#include <string>
+#include <vector>
+
+using namespace acc::utils;
+using namespace acc::lexical_analyzer;
+
+BOOST_AUTO_TEST_SUITE(lexical_analyzer)
+
+BOOST_AUTO_TEST_CASE(simple_main) {
+   std::vector<std::string> file_list = {"examples/main.c"};
+   std::vector<Token> tokens = tokenize_file(file_list);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
