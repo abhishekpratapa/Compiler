264ae75d628570fd5b3bd36e69c44f07d92f6592
Abhishek Pratapa <abhishekpratapa@gmail.com>
2019-07-22 23:48:25 -0700

    Added test to check for size of tokens

--- test/test_lexical_analyzer.cc
+++ test/test_lexical_analyzer.cc
@@ -15,8 +15,9 @@ using namespace acc::lexical_analyzer;
 BOOST_AUTO_TEST_SUITE(lexical_analyzer)
 
 BOOST_AUTO_TEST_CASE(simple_main) {
-   std::vector<std::string> file_list = {"examples/main.c"};
-   std::vector<Token> tokens = tokenize_file(file_list);
+  std::vector<std::string> file_list = {"examples/main.c"};
+  std::vector<Token> tokens = tokenize_file(file_list);
+  BOOST_CHECK_EQUAL(tokens.size(), 10);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
